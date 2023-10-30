09b3af5820740f129dd191130dabfcf041deb841
Ömer Yılmaz <omogalatasaray@gmail.com>
2023-10-30 23:16:16 +0300

    65.ders guncelleme

--- BusinessLayer/Container/Extensions.cs
+++ BusinessLayer/Container/Extensions.cs
@@ -30,6 +30,10 @@ namespace BusinessLayer.Container
             services.AddScoped<IGuideService, GuideManager>();
             services.AddScoped<IGuideDal, EfGuideDal>();
 
+            services.AddScoped<IExcelService, ExcelManager>();
+
+            services.AddScoped<IPdfService, PdfManager>();
+
         }
     }
 }
